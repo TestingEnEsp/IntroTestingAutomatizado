15ac4b1a8aff9e072bc60b7010de04679c9bbfa5
TestingEnEsp <testingenesp@gmail.com>
2015-08-01 17:11:29 -0300

    Create 01_search_product_con_setup.py

---
+++ Python/search_product/01_search_product_con_setup.py
@@ -0,0 +1,26 @@
+import unittest
+from selenium import webdriver
+
+# En este caso incluimos el Framework unittest de python para correr pruebas 
+# y agregar un setup a las mismas.
+
+class SearchTests(unittest.TestCase):
+    def setUp(self):
+        # Instanciar Firefox
+        self.driver = webdriver.Firefox()
+        self.driver.maximize_window()
+
+        # Navegamos hasta la aplicacion
+        self.driver.get("http://demo.magentocommerce.com/")
+
+    def test_search_by_category(self):
+        # Creamos el WebElement Search Field
+        self.search_field = self.driver.find_element_by_id("search")
+        # Introducimos la busqueda
+        self.search_field.send_keys("phones")
+        self.search_field.submit()
+        # self.driver.find_element_by_class_name("search-button").click()
+
+        # Capturamos todos los elementos que devuelve la busqueda
+        products = self.driver.find_elements_by_css_selector(".product-name a")
+        self.assertEqual(2, len(products))
