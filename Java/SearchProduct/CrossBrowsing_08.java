fd719aec00e8996348328b64248cf15fab956e92
TestingEnEsp <testingenesp@gmail.com>
2015-08-01 22:56:27 -0300

    Create CrossBrowsing_08.java

---
+++ Java/SearchProduct/CrossBrowsing_08.java
@@ -0,0 +1,56 @@
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+// Vemos como configurar el test para correr otros browsers en forma local
+
+public class CrossBrowsing_08 {
+		
+	WebDriver driver;
+	
+	@Before
+	public void SetUp() {
+		// Le indicamos donde se ubica el chromedriver.exe 
+		System.setProperty("webdriver.chrome.driver", 
+				"/opt/pythonenv/WebDriver/introTestingAutomatizado/search_product/search_product/chromedriver.exe");
+		// Instanciamos Chrome
+		driver = new ChromeDriver();
+
+		// Para el caso de IE, seria asi
+		// Le indicamos donde se ubica el IeDriver.exe 
+		//System.setProperty("webdriver.internetexplorer.driver", 
+				"/opt/pythonenv/WebDriver/introTestingAutomatizado/search_product/search_product/internetexplorer.exe");
+		// Instanciamos IE
+		// driver = new InternetExplorerDriver()
+
+		driver.manage().window().maximize();
+		driver.get("http://demo.magentocommerce.com/");
+	}
+	
+	@Test
+	public void testSearchByCategory() {
+
+		WebElement searchField = driver.findElement(By.id("search"));
+		
+		searchField.sendKeys("phones");
+		
+		searchField.submit();
+		//driver.findElement(By.className("search-button")).click();
+		
+		List<WebElement> products = driver.findElements(By.cssSelector(".product-name a"));
+		
+		assert products.size() == 2;
+	}
+	
+	@After
+	public void TearDown() {
+		driver.quit();
+	}
+
+}
