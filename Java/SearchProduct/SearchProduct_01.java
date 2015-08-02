0966d95bf5ce8ed557bc83a1e5e8d5119a2981cd
TestingEnEsp <testingenesp@gmail.com>
2015-08-01 22:42:24 -0300

    Update and rename SearchProduct_00.java to SearchProduct_01.java

--- Java/SearchProduct/SearchProduct_01.java
+++ Java/SearchProduct/SearchProduct_01.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class SearchProduct_00 {
+public class SearchProduct_01 {
 	public static void main(String[] args) {
 		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
