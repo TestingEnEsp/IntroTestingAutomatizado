9d77afa24b1ab773f9c8fc7dca7bfeb998721f6d
José Pablo Sarco <testingenesp@gmail.com>
2015-08-12 16:30:09 -0300

    Create search_page.py

---
+++ Python/page_object/02_movemos_intenciones/search_page.py
@@ -0,0 +1,8 @@
+
+class SearchPage(object):
+
+    def __init__(self, driver):
+        self._driver = driver
+
+    def get_cant_resultados(self):
+        return len(self._driver.find_elements_by_css_selector(".product-name a"))
