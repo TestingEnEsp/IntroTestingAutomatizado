0321218fedd9bd1ec349040b20c30cd92c279e22
José Pablo Sarco <testingenesp@gmail.com>
2015-08-13 12:27:29 -0300

    Create SearchPage.java

---
+++ Java/PageObject/06_PageBase/SearchPage.java
@@ -0,0 +1,15 @@
+package pagebase;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SearchPage extends PageBase {
+		
+	public SearchPage(WebDriver driver){
+		this._driver=driver;
+	}
+	
+	public int getCantResultados(){
+		return this._driver.findElements(By.cssSelector(".product-name a")).size();
+	}
+}
