15c44aae539f57a456ee7ae3a17d487cf992dcbf
José Pablo Sarco <testingenesp@gmail.com>
2015-08-13 12:28:01 -0300

    Create TestBase.java

---
+++ Java/PageObject/06_PageBase/TestBase.java
@@ -0,0 +1,23 @@
+package pagebase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestBase {
+	
+	WebDriver driver;
+	
+	@Before
+	public void SetUp() {
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("http://demo.magentocommerce.com/");
+	}
+	
+	@After
+	public void TearDown() {
+		driver.quit();
+	}
+}
