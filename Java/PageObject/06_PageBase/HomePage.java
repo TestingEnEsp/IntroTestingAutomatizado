e60f2f55738b0aa79256d56fed4648c3436261f2
José Pablo Sarco <testingenesp@gmail.com>
2015-08-13 12:19:56 -0300

    Create HomePage.java

---
+++ Java/PageObject/06_PageBase/HomePage.java
@@ -0,0 +1,21 @@
+package pagebase;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import sipageobject.SearchPage;
+
+public class HomePage extends PageBase{
+		
+	public HomePage(WebDriver driver) {
+		this._driver = driver;
+	}
+	
+	public SearchPage searchFor(String keyword){
+        WebElement search_field = this._driver.findElement(By.id("search"));
+        search_field.sendKeys(keyword);
+        search_field.submit();
+
+        return new SearchPage(this._driver);
+	}
+}
