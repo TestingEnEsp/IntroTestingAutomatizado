780e95707fd814764fa8cd809a2e08e182851853
José Pablo Sarco <testingenesp@gmail.com>
2015-08-14 12:59:04 -0300

    Update HomePage.java

--- Java/PageObject/04_MasTests/HomePage.java
+++ Java/PageObject/04_MasTests/HomePage.java
@@ -3,7 +3,7 @@ package mastests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import sipageobject.SearchPage;
+import mastests.SearchPage;
 
 public class HomePage {
 	
