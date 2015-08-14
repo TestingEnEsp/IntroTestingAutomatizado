1b4b762a06e6872a4109ee27647d4b836eaad721
José Pablo Sarco <testingenesp@gmail.com>
2015-08-14 13:00:19 -0300

    Update HomePage.java

--- Java/PageObject/05_TestBase/HomePage.java
+++ Java/PageObject/05_TestBase/HomePage.java
@@ -3,7 +3,7 @@ package testbase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import sipageobject.SearchPage;
+import testbase.SearchPage;
 
 public class HomePage {
 	
