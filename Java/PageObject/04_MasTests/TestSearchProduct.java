574ae1f89bb0ad57f57b1cc17362f7f87c714f6f
José Pablo Sarco <testingenesp@gmail.com>
2015-08-14 12:59:24 -0300

    Update TestSearchProduct.java

--- Java/PageObject/04_MasTests/TestSearchProduct.java
+++ Java/PageObject/04_MasTests/TestSearchProduct.java
@@ -6,8 +6,8 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import sipageobject.HomePage;
-import sipageobject.SearchPage;
+import mastests.HomePage;
+import mastests.SearchPage;
 
 public class TestSearchProduct {
 
