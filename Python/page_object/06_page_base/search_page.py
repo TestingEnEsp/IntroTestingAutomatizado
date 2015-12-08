92626db2e531b53ecc72dea9b8b9eba82b40a6d8
José Pablo Sarco <testingenesp@gmail.com>
2015-12-08 17:36:39 -0300

    Update search_page.py

--- Python/page_object/06_page_base/search_page.py
+++ Python/page_object/06_page_base/search_page.py
@@ -1,8 +1,6 @@
+from page_base import BasePage
 
-class SearchPage(object):
 
-    def __init__(self, driver):
-        self._driver = driver
-
+class SearchPage(BasePage):
     def get_cant_resultados(self):
-        return len(self._driver.find_elements_by_css_selector(".product-name a"))
+        return len(self._driver.find_elements_by_css_selector("#searchResults>li"))
