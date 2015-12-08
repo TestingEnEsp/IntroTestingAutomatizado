acbd5011638286c4d960c231e97f29fa1bd4e47c
José Pablo Sarco <testingenesp@gmail.com>
2015-12-08 14:22:58 -0300

    Update 01_search_product.py

--- Python/search_product/01_search_product.py
+++ Python/search_product/01_search_product.py
@@ -5,21 +5,21 @@ driver = webdriver.Firefox()
 driver.maximize_window()
 
 # Navegamos hacia la aplicacion
-driver.get("http://demo.magentocommerce.com/")
+driver.get("http://www.mercadolibre.com.ar/")
 
 # Creamos el WebElement Search Field
-search_field = driver.find_element_by_id("search")
+search_field = driver.find_element_by_id("query")
 
 # Introducimos la busqueda
-search_field.send_keys("phones")
+search_field.send_keys("telefonos")
 
 # Realizamos la busqueda
 search_field.submit()  # sin presionar el boton buscar
 # presionando buscar seria asi
-# driver.find_element_by_class_name("search-button").click()
+# driver.find_element_by_class_name("nav-search-btn.ml-search-btn").click()
 
 # Capturamos todos los elementos que devuelve la busqueda
-products = driver.find_elements_by_css_selector(".product-name a")
+products = driver.find_elements_by_css_selector("#searchResults>li")
 
 # Validamos la cantidad de resultados devueltos
-assert 2 == len(products), "2 != {0}".format(len(products))
+assert 50 == len(products), "50 != {0}".format(len(products))
