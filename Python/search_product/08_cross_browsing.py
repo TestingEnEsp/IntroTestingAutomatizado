e8637427833da4c89b13ad429491cea17b148a72
José Pablo Sarco <testingenesp@gmail.com>
2015-12-08 15:25:50 -0300

    Update 08_cross_browsing.py

--- Python/search_product/08_cross_browsing.py
+++ Python/search_product/08_cross_browsing.py
@@ -22,33 +22,32 @@ class SearchTests(unittest.TestCase):
         self.driver.implicitly_wait(30)
 
         # Navegamos hasta la aplicacion
-        self.driver.get("http://demo.magentocommerce.com/")
+        self.driver.get("http://www.mercadolibre.com.ar/")
 
     def test_search_by_category(self):
         # Creamos el WebElement Search Field
-        self.search_field = self.driver.find_element_by_id("search")
-
+        search_field = self.driver.find_element_by_id("query")
         # Introducimos la busqueda
-        self.search_field.send_keys("phones")
-        self.search_field.submit()
-        # self.driver.find_element_by_class_name("search-button").click()
+        search_field.send_keys("telefonos")
+        search_field.submit()
+        # self.driver.find_element_by_class_name("nav-search-btn.ml-search-btn").click()
 
         # Capturamos todos los elementos que devuelve la busqueda
-        products = self.driver.find_elements_by_css_selector(".product-name a")
-        self.assertEqual(2, len(products))
+        products = self.driver.find_elements_by_css_selector("#searchResults>li")
+        self.assertEqual(50, len(products))
 
     def test_search_by_name(self):
         # capturamos el search
-        self.search_field = self.driver.find_element_by_id("search")
-        self.search_field.clear()
+        search_field = self.driver.find_element_by_id("query")
+        search_field.clear()
 
         # Introducimos la busqueda
-        self.search_field.send_keys("salt shaker")
-        self.search_field.submit()
-        # self.driver.find_element_by_class_name("search-button").click()
+        search_field.send_keys("software testing: fundamental principles and essential know")
+        search_field.submit()
+        # self.driver.find_element_by_class_name("nav-search-btn.ml-search-btn").click()
 
         # Capturamos todos los elementos que devuelve la busqueda
-        products = self.driver.find_elements_by_css_selector(".product-name a")
+        products = self.driver.find_elements_by_css_selector("#searchResults>li")
         self.assertEqual(1, len(products))
 
     def tearDown(self):
