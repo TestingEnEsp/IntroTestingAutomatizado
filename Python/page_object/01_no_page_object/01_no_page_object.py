06273b5d75ff1f37ff727d675175b45912dea212
José Pablo Sarco <testingenesp@gmail.com>
2015-12-08 16:10:18 -0300

    Update 01_no_page_object.py

--- Python/page_object/01_no_page_object/01_no_page_object.py
+++ Python/page_object/01_no_page_object/01_no_page_object.py
@@ -1,55 +1,42 @@
 import unittest
 from selenium import webdriver
 
+
 class SearchTests(unittest.TestCase):
     def setUp(self):
         # Instanciar Firefox
         self.driver = webdriver.Firefox()
         self.driver.maximize_window()
 
+        self.driver.implicitly_wait(30)
+
         # Navegamos hasta la aplicacion
-        self.driver.get("http://demo.magentocommerce.com/")
+        self.driver.get("http://www.mercadolibre.com.ar/")
 
     def test_search_by_category(self):
         # Creamos el WebElement Search Field
-        self.search_field = self.driver.find_element_by_id("search")
+        search_field = self.driver.find_element_by_id("query")
         # Introducimos la busqueda
-        self.search_field.send_keys("phones")
-        self.search_field.submit()
-        # self.driver.find_element_by_class_name("search-button").click()
+        search_field.send_keys("telefonos")
+        search_field.submit()
+        # self.driver.find_element_by_class_name("nav-search-btn.ml-search-btn").click()
 
         # Capturamos todos los elementos que devuelve la busqueda
-        products = self.driver.find_elements_by_css_selector(".product-name a")
-        self.assertEqual(2, len(products))
+        products = self.driver.find_elements_by_css_selector("#searchResults>li")
+        self.assertEqual(50, len(products))
 
-    def test_search_by_product(self):
+    def test_search_by_name(self):
         # Creamos el WebElement Search Field
-        self.search_field = self.driver.find_element_by_id("search")
+        search_field = self.driver.find_element_by_id("query")
         # Introducimos la busqueda
-        self.search_field.send_keys("Oxford")
-        self.search_field.submit()
-        # self.driver.find_element_by_class_name("search-button").click()
+        search_field.send_keys("software testing: fundamental principles and essential know")
+        search_field.submit()
+        # self.driver.find_element_by_class_name("nav-search-btn.ml-search-btn").click()
 
         # Capturamos todos los elementos que devuelve la busqueda
-        products = self.driver.find_elements_by_css_selector(".product-name a")
-        self.assertEqual(3, len(products))
-
-    def test_no_results(self):
-        # Creamos el WebElement Search Field
-        self.search_field = self.driver.find_element_by_id("search")
-        # Introducimos la busqueda
-        self.search_field.send_keys("accesories")
-        self.search_field.submit()
-        # self.driver.find_element_by_class_name("search-button").click()
-
-        # Capturamos todos los elementos que devuelve la busqueda
-        products = self.driver.find_elements_by_css_selector(".product-name a")
-        self.assertEqual(0, len(products))
+        products = self.driver.find_elements_by_css_selector("#searchResults>li")
+        self.assertEqual(1, len(products))
 
     def tearDown(self):
         # Cerramos el navegador
         self.driver.quit()
-
-
-if __name__ == '__main__':
-    unittest.main(verbosity=2)
