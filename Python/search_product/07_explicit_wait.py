49a9fd5107b153a144c2f817ad37f7668742b95f
José Pablo Sarco <testingenesp@gmail.com>
2015-12-08 15:07:22 -0300

    Update 07_explicit_wait.py

--- Python/search_product/07_explicit_wait.py
+++ Python/search_product/07_explicit_wait.py
@@ -10,34 +10,14 @@ class ExplicitWaitTests(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.maximize_window()
-        self.driver.get("http://demo.magentocommerce.com/")
+        self.driver.get("http://www.mercadolibre.com.ar/")
 
-    def test_account_link(self):
-        # Capturamos el elemento account cuando se cumple la condicion de que esta visible
-        account = WebDriverWait(self.driver, 10)\
-            .until(expected_conditions.visibility_of_element_located((By.CLASS_NAME, "skip-account")))
-        account.click()
-
-    def test_create_new_customer(self):
-        # Hacemos click en Account para habilitar el link de Login
-        self.driver.find_element_by_class_name("skip-account").click()
-
-        # Esperamos por el link y le hacemos click
-        my_account = WebDriverWait(self.driver, 10)\
-            .until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, "#header-account .first a")))
-        my_account.click()
-
-        # Capturamos el webelement Create account
-        create_account_button = WebDriverWait(self.driver, 10)\
-            .until(expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, ".button[title='Create an Account']")))
-
-        # Hacemos click y esperamos por el titulo de la pantalla
-        create_account_button.click()
-        WebDriverWait(self.driver, 10)\
-            .until(expected_conditions.title_contains("Create New Customer Account"))
+    def test_suggestions(self):
+        # Creamos el WebElement Search Field
+        search_field = self.driver.find_element_by_id("query")
+        search_field.send_keys("tele")
+        suggestions = WebDriverWait(self.driver, 10).until(expected_conditions.visibility_of_element_located((By.ID, "ac-popover-2")))
+        suggestions.find_element_by_class_name("ac-autocomplete-item").click()
 
     def tearDown(self):
         self.driver.quit()
-
-if __name__ == "__main__":
-    unittest.main(verbosity=2)
