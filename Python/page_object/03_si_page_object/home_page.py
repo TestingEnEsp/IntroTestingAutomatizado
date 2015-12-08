5db5318e7ffbc81bb18e411db569a0f45f955d1e
José Pablo Sarco <testingenesp@gmail.com>
2015-12-08 17:28:44 -0300

    Update home_page.py

--- Python/page_object/03_si_page_object/home_page.py
+++ Python/page_object/03_si_page_object/home_page.py
@@ -1,17 +1,15 @@
 from search_page import SearchPage
 
+
 class HomePage(object):
 
     def __init__(self, driver):
         self._driver = driver
-        self._url = 'http://demo.magentocommerce.com/'
-        self._title = 'Madison Island'
-
-    def navigate_to(self):
-        self._driver.get(self._url)
+        self._url = 'http://www.mercadolibre.com.ar/'
+        self._title = 'MercadoLibre Argentina'
 
     def search_for(self, keyword):
-        search_field = self._driver.find_element_by_id("search")
+        search_field = self._driver.find_element_by_id("query")
         search_field.send_keys(keyword)
         search_field.submit()
 
