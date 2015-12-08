324cbfd7ffc1b3b0ae071f0f6ff1d24b3eb414c4
José Pablo Sarco <testingenesp@gmail.com>
2015-12-08 17:44:20 -0300

    Update home_page.py

--- Python/page_object/07_sauce_labs/home_page.py
+++ Python/page_object/07_sauce_labs/home_page.py
@@ -1,13 +1,11 @@
 from search_page import SearchPage
 from page_base import BasePage
 
-class HomePage(BasePage):
 
-    def navigate_to(self):
-        self._driver.get(self._url)
+class HomePage (BasePage):
 
     def search_for(self, keyword):
-        search_field = self._driver.find_element_by_id("search")
+        search_field = self._driver.find_element_by_id("query")
         search_field.send_keys(keyword)
         search_field.submit()
 
