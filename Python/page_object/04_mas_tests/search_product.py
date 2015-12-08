3867bea1d465b42364a5744e3c44192270e645eb
José Pablo Sarco <testingenesp@gmail.com>
2015-12-08 17:34:09 -0300

    Update search_product.py

--- Python/page_object/04_mas_tests/search_product.py
+++ Python/page_object/04_mas_tests/search_product.py
@@ -12,22 +12,22 @@ class SearchTests(unittest.TestCase):
         self.driver.get("http://demo.magentocommerce.com/")
 
     def test_search_by_category(self):
+        # Instanciamos la Home Page
         home_page = HomePage(self.driver)
-        results_page = home_page.search_for("phones")
-
-        self.assertEqual(2, results_page.get_cant_resultados())
-
-    def test_search_by_product(self):
+        # Hacemos la busqueda desde la Home Page. Esta busqueda nos devuelve una Results Page
+        results_page = home_page.search_for("telefonos")
+        
+        # Verificamos la cantidad de resultados
+        self.assertEqual(50, results_page.get_cant_resultados())
+        
+    def test_search_by_name(self):
+        # Instanciamos la Home Page
         home_page = HomePage(self.driver)
-        results_page = home_page.search_for("Oxford")
-
-        self.assertEqual(3, results_page.get_cant_resultados())
-
-    def test_no_results(self):
-        home_page = HomePage(self.driver)
-        results_page = home_page.search_for("accesories")
-
-        self.assertEqual(0, results_page.get_cant_resultados())
+        # Hacemos la busqueda desde la Home Page. Esta busqueda nos devuelve una Results Page
+        results_page = home_page.search_for("software testing: fundamental principles and essential know")
+        
+        # Verificamos la cantidad de resultados
+        self.assertEqual(1, results_page.get_cant_resultados())
 
     def tearDown(self):
         # Cerramos el navegador
