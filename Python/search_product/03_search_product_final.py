e3ce2f780cec68a015e35286e3d29bccdea04b61
José Pablo Sarco <testingenesp@gmail.com>
2015-12-08 14:25:47 -0300

    Update 03_search_product_final.py

--- Python/search_product/03_search_product_final.py
+++ Python/search_product/03_search_product_final.py
@@ -11,19 +11,19 @@ class SearchTests(unittest.TestCase):
         self.driver.maximize_window()
 
         # Navegamos hasta la aplicacion
-        self.driver.get("http://demo.magentocommerce.com/")
+        self.driver.get("http://www.mercadolibre.com.ar/")
 
     def test_search_by_category(self):
         # Creamos el WebElement Search Field
-        self.search_field = self.driver.find_element_by_id("search")
+        search_field = self.driver.find_element_by_id("query")
         # Introducimos la busqueda
-        self.search_field.send_keys("phones")
-        self.search_field.submit()
-        # self.driver.find_element_by_class_name("search-button").click()
+        search_field.send_keys("telefonos")
+        search_field.submit()
+        # self.driver.find_element_by_class_name("nav-search-btn.ml-search-btn").click()
 
         # Capturamos todos los elementos que devuelve la busqueda
-        products = self.driver.find_elements_by_css_selector(".product-name a")
-        self.assertEqual(2, len(products))
+        products = self.driver.find_elements_by_css_selector("#searchResults>li")
+        self.assertEqual(50, len(products))
 
     def tearDown(self):
         # Cerramos el navegador
