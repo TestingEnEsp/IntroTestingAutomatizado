c96e523d9bcb95bbfd7fe8a7540d714dcc73e315
José Pablo Sarco <testingenesp@gmail.com>
2015-12-16 23:06:14 -0300

    Update HomePage.java

--- Java/PageObject/02_MovemosIntenciones/HomePage.java
+++ Java/PageObject/02_MovemosIntenciones/HomePage.java
@@ -1,22 +1,22 @@
-package movemosintenciones_02;
+package 02_movemosintenciones;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import movemosintenciones_02.SearchPage;
+import movemosintenciones.SearchPage;
 
 public class HomePage {
 	
 	WebDriver _driver;
-	String _url = "http://demo.magentocommerce.com/";
-	String _title = "Madison Island";
+	String _url = "http://www.mercadolibre.com.ar/";
+	String _title = "MercadoLibre Argentina";
 	
 	public HomePage(WebDriver driver) {
 		this._driver = driver;
 	}
 	
 	public SearchPage searchFor(String keyword){
-        WebElement search_field = this._driver.findElement(By.id("search"));
+        WebElement search_field = this._driver.findElement(By.id("query"));
         search_field.sendKeys(keyword);
         search_field.submit();
 
