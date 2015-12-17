7a7196bd0c023402579fd90c7591e01531add44b
José Pablo Sarco <testingenesp@gmail.com>
2015-12-16 23:38:39 -0300

    Update HomePage.java

--- Java/PageObject/05_TestBase/HomePage.java
+++ Java/PageObject/05_TestBase/HomePage.java
@@ -1,22 +1,22 @@
-package testbase;
+package 05_TestBase;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import testbase.SearchPage;
+import pagebase.SearchPage;
 
 public class HomePage {
 	
 	WebDriver _driver;
-	String _url = "http://demo.magentocommerce.com/";
-	String _title = "Madison Island";
+	String _url = "http://www.mercadolibre.com.ar/";
+	String _title = "MercadoLibre Argentina";
 	
 	public HomePage(WebDriver driver) {
 		this._driver = driver;
 	}
 	
 	public SearchPage searchFor(String keyword){
-        WebElement search_field = this._driver.findElement(By.id("search"));
+        WebElement search_field = this._driver.findElement(By.id("query"));
         search_field.sendKeys(keyword);
         search_field.submit();
 
