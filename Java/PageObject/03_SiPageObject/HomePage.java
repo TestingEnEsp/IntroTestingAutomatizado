4486e499aed6878b6bc38bc5c7ae57c8b6b77c7b
José Pablo Sarco <testingenesp@gmail.com>
2015-12-16 23:20:04 -0300

    Update HomePage.java

--- Java/PageObject/03_SiPageObject/HomePage.java
+++ Java/PageObject/03_SiPageObject/HomePage.java
@@ -1,4 +1,4 @@
-package sipageobject;
+package 03_SiPageObject;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,15 +8,15 @@ import sipageobject.SearchPage;
 public class HomePage {
 	
 	WebDriver _driver;
-	String _url = "http://demo.magentocommerce.com/";
-	String _title = "Madison Island";
+	String _url = "http://www.mercadolibre.com.ar/";
+	String _title = "MercadoLibre Argentina";
 	
 	public HomePage(WebDriver driver) {
 		this._driver = driver;
 	}
 	
 	public SearchPage searchFor(String keyword){
-        WebElement search_field = this._driver.findElement(By.id("search"));
+        WebElement search_field = this._driver.findElement(By.id("query"));
         search_field.sendKeys(keyword);
         search_field.submit();
 
