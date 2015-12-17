ed1cb5ff701b39872d6136b6d1e9504a836f67af
José Pablo Sarco <testingenesp@gmail.com>
2015-12-16 23:51:47 -0300

    Update SearchPage.java

--- Java/PageObject/06_PageBase/SearchPage.java
+++ Java/PageObject/06_PageBase/SearchPage.java
@@ -1,15 +1,16 @@
-package pagebase;
+package 06_PageBase;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class SearchPage extends PageBase {
+public class SearchPage extends PageBase{
 		
 	public SearchPage(WebDriver driver){
 		this._driver=driver;
 	}
 	
 	public int getCantResultados(){
-		return this._driver.findElements(By.cssSelector(".product-name a")).size();
+		return this._driver.findElements(By.cssSelector("#searchResults>li")).size();
 	}
+
 }
